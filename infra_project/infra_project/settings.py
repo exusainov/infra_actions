13b8166aed0f80f4ec3dcfe748946b5aac80c7dd
Женя Хусаинов <evares92@gmail.com>
2022-08-05 21:05:57 +0500

    Тестирование dockerfile

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['130.193.40.89', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.18.172', '127.0.0.1', 'localhost']
 
 
 # Application definition
